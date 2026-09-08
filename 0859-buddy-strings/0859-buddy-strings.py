ad620dee180cd8fd4d63e0535d51393210784f4c
PRINCE KHATRI <princekumarkhatri07@gmail.com>
2026-09-08 09:47:38 +0530

    Time: 3 ms (54.69%), Space: 13.1 MB (81.25%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def buddyStrings(self, s, goal):
+        if len(s) != len(goal):
+            return False
+        if s==goal:
+            return len(set(s)) < len(s)
+
+        diff = []
+        for i in range(len(s)):
+            if s[i] != goal[i]:
+                diff.append(i)
+
+
+        if len(diff) != 2:
+            return False
+
+        i, j = diff
+        return s[i] == goal[j] and s[j] == goal[i]
+
+        
+
+        
+
+       
